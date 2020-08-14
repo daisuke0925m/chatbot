8644baf6e534b9e96b812f40450397568a8e2e4a
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-08-14 11:03:47 +0900

    updated

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -5,10 +5,10 @@ const AnswersList = (props) => {
   return (
     <div className="c-grid__answer">
       {props.answers.map((value, index) => {
-        return <Answer content={value.content} />
+        return <Answer content={value.content} key={index.toString()} />
       })}
     </div>
-  ) 
+  )
 }
 
 export default AnswersList;
